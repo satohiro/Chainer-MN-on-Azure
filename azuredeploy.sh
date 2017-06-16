eafb7bf10f6f7df56ddb3e0ce45d959adc1a23a7
Hiroyuki Sato <hiroyuki.sato@imsbio.co.jp>
2017-06-16 17:21:28 +0900

    fix for GPU node

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -18,9 +18,15 @@ create_newuser_on_nfsserver() {
   chmod 600 ${SSHDIR}/config
   chown -R ${NEWUSER}. /home/${NEWUSER}
 
-  #setup .bashrc for chanermn CPU
+  #setup .bashrc for chanermn
+  echo "export PATH=\"/usr/local/cuda/bin:\$PATH\"" >> /home/${NEWUSER}/.bashrc
+  echo "export PATH=\"/usr/local/cuda/bin:\$PATH\"" > /tmp/bashrc.$$
+  echo "export CPATH=\"/usr/local/cuda/include:\$CPATH\"" >> /home/${NEWUSER}/.bashrc
+  echo "export CPATH=\"/usr/local/cuda/include:\$CPATH\"" >> /tmp/bashrc.$$
+  echo "export LD_LIBRARY_PATH=\"/usr/local/cuda/lib64:\$LD_LIBRARY_PATH\"" >> /home/${NEWUSER}/.bashrc
+  echo "export LD_LIBRARY_PATH=\"/usr/local/cuda/lib64:\$LD_LIBRARY_PATH\"" >> /tmp/bashrc.$$
   echo "export MPI_ROOT=\"/usr/local/lib/openmpi\"" >> /home/${NEWUSER}/.bashrc
-  echo "export MPI_ROOT=\"/usr/local/lib/openmpi\"" > /tmp/bashrc.$$
+  echo "export MPI_ROOT=\"/usr/local/lib/openmpi\"" >> /tmp/bashrc.$$
   echo "export CPATH=\"/usr/local/include/openmpi:\$CPATH\"" >> /home/${NEWUSER}/.bashrc
   echo "export CPATH=\"/usr/local/include/openmpi:\$CPATH\"" >> /tmp/bashrc.$$
   echo "export LD_LIBRARY_PATH=\"/usr/local/lib/openmpi:\$LD_LIBRARY_PATH\"" >> /home/${NEWUSER}/.bashrc
@@ -50,20 +56,71 @@ create_etc_hosts() {
 }
 
 setup_chainermn() {
-  ### THIS VERSION is CPU only ###
+  # check GUP device
+  NVIDIA_DEVICE=$(lspci | grep -i NVIDIA)
+  echo "$NVIDIA_DEVICE" > /tmp/gpu_check.$$
+  if [ -z "$NVIDIA_DEVICE"]; then
+    echo "CPU node" >> /tmp/gpu_check.$$
+  fi
+  if [ -n "$NVIDIA_DEVICE"]; then
+    echo "GPU node" >> /tmp/gpu_check.$$
+  fi
+
+  #setup cuda (for GPU node)
+  if [ -n "$NVIDIA_DEVICE"]; then
+    CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+    sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+    sudo apt-get update
+    sudo apt-get install -y cuda-drivers
+    sudo apt-get install -y cuda
+	sudo nvidia-smi -pm 1 > /tmp/nvidia-smi.$$ 2>&1
+  fi
 
   # setup libs
   sudo apt-get install -y git vim build-essential python-dev libgtk2.0-dev tmux byobu python-pip python3-pip libffi-dev
 
+  # setup cudnn (for GPU node)
+  if [ -n "$NVIDIA_DEVICE"]; then
+    cd /tmp
+    curl http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz > cudnn-8.0-linux-x64-v6.0.tgz
+    tar -zxf cudnn-8.0-linux-x64-v6.0.tgz
+    sudo cp -a cuda/lib64/* /usr/local/cuda-8.0/lib64/
+    sudo cp -a cuda/include/* /usr/local/cuda-8.0/include/
+	# Bug fix start --
+	#  reference https://askubuntu.com/questions/900285/libegl-so-1-is-not-a-symbolic-link
+	sudo mv /usr/lib/nvidia-375/libEGL.so.1 /usr/lib/nvidia-375/libEGL.so.1.org
+    sudo mv /usr/lib32/nvidia-375/libEGL.so.1 /usr/lib32/nvidia-375/libEGL.so.1.org
+    sudo ln -s /usr/lib/nvidia-375/libEGL.so.375.39 /usr/lib/nvidia-375/libEGL.so.1
+    sudo ln -s /usr/lib32/nvidia-375/libEGL.so.375.39 /usr/lib32/nvidia-375/libEGL.so.1
+	# Bug fix end -- 
+    sudo ldconfig > /tmp/ldconfig_cudnn.$$ 2>&1
+  fi
+
+  # setup nccl (for GPU node)
+  if [ -n "$NVIDIA_DEVICE"]; then
+    cd /tmp
+    mkdir nccl
+    cd nccl
+    git clone https://github.com/NVIDIA/nccl.git .
+    make CUDA_HOME=/usr/local/cuda > /tmp/nccl.$$ 2>&1
+    sudo make install >> /tmp/nccl.$$ 2>&1
+  fi
+
   # setup openmpi
+  OPENMPI_CUDA_OPTION=""
+  if [ -n "$NVIDIA_DEVICE"]; then
+    OPENMPI_CUDA_OPTION="--with-cuda"
+  fi
+  echo $OPENMPI_CUDA_OPTION > /tmp/openmpi.$$ 2>&1
   cd /tmp
   mkdir -p /tmp/openmpi
   cd /tmp/openmpi
   wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.0.tar.gz
   tar zxf openmpi-2.1.0.tar.gz
   cd openmpi-2.1.0
-  ./configure > /tmp/openmpi.$$ 2>&1
-  make all >> /tmp/openmpi.$$ 2>&1
+  ./configure $OPENMPI_CUDA_OPTION >> /tmp/openmpi.$$ 2>&1
+  make -j4 all >> /tmp/openmpi.$$ 2>&1
   sudo make install >> /tmp/openmpi.$$ 2>&1
 
   # setup hostfile
@@ -78,13 +135,19 @@ setup_chainermn() {
 
   # setup cython
   sudo pip3 install cython > /tmp/cython.$$ 2>&1
-  sudo ldconfig > /tmp/ldconfig.$$ 2>&1
+  sudo ldconfig > /tmp/ldconfig_cython.$$ 2>&1
 
-  # setup chainermn for CPU
-  cd /tmp
-  git clone https://github.com/pfnet/chainermn
-  cd chainermn
-  LDFLAGS="-L/usr/local/lib/openmpi -L/usr/local/lib" CFLAGS="-I/usr/local/cuda/include -I/usr/local/include" sudo python3 setup.py install --no-nccl  > /tmp/chainermn.$$ 2>&1
+  # setup chainermn
+  if [ -n "$NVIDIA_DEVICE"]; then
+    # for GPU node
+	LDFLAGS="-L/usr/local/lib/openmpi -L/usr/local/lib" CFLAGS="-I/usr/local/cuda/include -I/usr/local/include" pip3 install git+https://github.com/pfnet/chainermn >> /tmp/chainermn.$$ 2>&1
+  else
+    # for CPU node
+    cd /tmp
+    git clone https://github.com/pfnet/chainermn
+    cd chainermn
+    LDFLAGS="-L/usr/local/lib/openmpi -L/usr/local/lib" CFLAGS="-I/usr/local/cuda/include -I/usr/local/include" python3 setup.py install --no-nccl >> /tmp/chainermn.$$ 2>&1
+  fi
  
  }
 
